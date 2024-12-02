71ec18ddd6bae550b66e37834872ed1a482df84e
Martin Tveten <tveten@nr.no>
2024-12-02 21:07:10 +0100

    skchange content (#1)
    Adds the content on `skchange`
    
    # Agenda so far
    
    1. Detection
    
        * Different tasks/taxonomy
        * Conceptual model
        * Interface
    
    2. `skchange` algorithm framework
    
        * Interval scoring
        * Search
    
    3. Interval scoring
    
        * Costs
        * Change scores
        * Anomaly scores
    
    4. Change detection
    
    5. Segment anomaly detection
    
    6. Air handling unit data example

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,8 @@
 from typing import Tuple
 
+import copy
 import pandas as pd
+import plotly.express as px
 
 
 def load_stallion(as_period=False) -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -12,3 +14,162 @@ def load_stallion(as_period=False) -> Tuple[pd.DataFrame, pd.DataFrame]:
     y = data[["volume"]]
     X = data.drop(columns="volume")
     return X, y
+
+
+def plot_multivariate_time_series(df):
+    plot_df = (
+        df.stack()
+        .reset_index()
+        .rename({"level_0": "index", "level_1": "variable", 0: "value"}, axis=1)
+    )
+    fig = px.line(plot_df, x="index", y="value", facet_row="variable")
+    return fig
+
+
+def add_changepoint_vlines(fig, changepoints):
+    fig = copy.deepcopy(fig)
+    for changepoint in changepoints:
+        fig.add_vline(x=changepoint, line_dash="dash", line_color="red")
+    return fig
+
+
+def add_segmentation_vrects(
+    fig, segments, segment_labels=None, colors=px.colors.qualitative.Alphabet
+):
+    fig = copy.deepcopy(fig)
+    if segment_labels is None:
+        segment_labels = pd.RangeIndex(len(segments))
+
+    if len(segments) != len(segment_labels):
+        raise ValueError("segments and segment_labels must be the same length")
+
+    if segment_labels.dtype != int:
+        raise ValueError("segment_labels must be of type int")
+
+    for segment, segment_label in zip(segments.values, segment_labels.values):
+        color = colors[segment_label % len(colors)]
+        fig.add_vrect(
+            x0=segment.left,
+            x1=segment.right,
+            fillcolor=color,
+            opacity=0.2,
+            line_width=0,
+            layer="below",
+        )
+    return fig
+
+
+def add_subset_segment_anomaly_vrects(fig, subset_anomalies):
+    fig = copy.deepcopy(fig)
+    n_vars = len(fig.data)
+    for row in subset_anomalies.itertuples():
+        columns = row.anomaly_columns
+        for col in columns:
+            fig.add_vrect(
+                x0=row.anomaly_interval.left,
+                x1=row.anomaly_interval.right,
+                fillcolor="red",
+                opacity=0.2,
+                line_width=0,
+                layer="below",
+                row=n_vars
+                - col,  # plotly rows are 1-indexed and we want to reverse the order
+                col=1,
+            )
+    return fig
+
+
+def to_time_intervals(intervals: pd.Series, times: pd.Index) -> pd.Series:
+    time_intervals = pd.Series(
+        [
+            pd.Interval(
+                times[interval.left],
+                times[interval.right - 1] + pd.Timedelta("10min"),
+                closed="left",
+            )
+            for interval in intervals
+        ],
+        name="interval",
+    )
+    return time_intervals
+
+
+def plot_changepoint_illustration(df, cpts, base_width=800, base_height=400):
+    cpt_fig = plot_multivariate_time_series(df)
+    cpt_fig = add_changepoint_vlines(cpt_fig, cpts)
+    for i, cpt in enumerate(cpts):
+        cpt_fig.add_annotation(
+            x=cpt,
+            y=-0.13,
+            text=f"changepoint {i+1}",
+            showarrow=False,
+            yshift=-10,
+            font=dict(size=16),
+            xref="x",
+            yref="paper",
+        )
+    cpt_fig.update_layout(
+        showlegend=False, xaxis_title=None, width=base_width, height=base_height
+    )
+    return cpt_fig
+
+
+def plot_segmentation_illustration(
+    df, segments, segment_labels, base_width=800, base_height=400
+):
+    segment_fig = plot_multivariate_time_series(df)
+    segment_fig = add_segmentation_vrects(segment_fig, segments, segment_labels)
+    for i, segment in enumerate(segments):
+        segment_fig.add_annotation(
+            x=segment.mid,
+            y=-0.22,
+            text=f"segment {i}<br> label {segment_labels[i]}",
+            showarrow=False,
+            yshift=-10,
+            font=dict(size=16),
+            xref="x",
+            yref="paper",
+        )
+    segment_fig.update_layout(
+        showlegend=False, xaxis_title=None, width=base_width, height=base_height
+    )
+    return segment_fig
+
+
+def plot_point_anomaly_illustration(
+    df, point_anomalies, base_width=800, base_height=400
+):
+    outlier_plot = plot_multivariate_time_series(df)
+    outlier_plot.add_scatter(
+        x=point_anomalies,
+        y=df.iloc[point_anomalies, 0],
+        mode="markers",
+        marker=dict(symbol="x", size=10, color="red"),
+        name="Point anomaly",
+    ).update_layout(width=base_width, height=base_height)
+    return outlier_plot
+
+
+def plot_segment_anomaly_illustration(df, anomalies, base_width=800, base_height=400):
+    anomaly_segments = pd.Series(
+        [pd.Interval(*anomaly, closed="left") for anomaly in anomalies]
+    )
+    anomaly_plot = plot_multivariate_time_series(df)
+    anomaly_plot = add_segmentation_vrects(
+        anomaly_plot, anomaly_segments, colors=["red"]
+    )
+    for i, segment in enumerate(anomaly_segments):
+        anomaly_plot.add_annotation(
+            x=segment.mid,
+            y=-0.13,
+            text=f"Segment anomaly {i+1}",
+            showarrow=False,
+            yshift=-10,
+            font=dict(size=16),
+            xref="x",
+            yref="paper",
+        )
+    anomaly_plot.update_layout(
+        showlegend=False, xaxis_title=None, width=base_width, height=base_height
+    )
+    return anomaly_plot
